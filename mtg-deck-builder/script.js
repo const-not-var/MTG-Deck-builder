9bb0b0e0909f18e741d0afe84488e3248641cc61
const-not-var <161810593+const-not-var@users.noreply.github.com>
2024-05-16 00:11:04 -0500

    Initial commit

---
+++ mtg-deck-builder/script.js
@@ -0,0 +1,30 @@
+const dataCardTemplate = document.querySelector("[data-card-template]")
+
+
+function clickTest() {
+    console.log("I am clicked");
+    document.getElementById("deckTester").innerText = "I am changed";
+    window.location.replace("/mydecks.html")
+}
+
+// function signup() {
+//     const email = document.getElementById("inputEmail3").value;
+//     const password = document.getElementById("inputPassword3").value;
+
+//     if (email && password) {
+//         window.location.replace("/index.html")
+//     } else {
+//         alert("NO!")
+//     }
+// }
+
+function login() {
+    const email = document.getElementById("inputEmail3").value;
+    const password = document.getElementById("inputPassword3").value;
+    
+    if (email === "mtg@dev.com" && password === "password") {
+        window.location.replace("/index.html")
+    } else {
+        alert("EMAIL OR PASSWORD INCORRECT!")
+    }
+}
---
+++ mtg-deck-builder/test.js
@@ -0,0 +1,121 @@
+// function searchBar() {
+//     fetch('https://api.magicthegathering.io/v1/cards')
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Network response is not ok');
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log('Data received:', data);
+//     })
+//     .catch(error => {
+//         console.error('Could not fetch data:', error);
+//     });
+// const searchInput = document.getElementById('exampleDataList');
+// searchInput.addEventListener('keyup', function(event) {
+//     if (KeyboardEvent.code === 13) {
+//       searchInput.click();
+//     }
+//   });
+// }
+
+
+    const dataCardTemplate = document.querySelector("[data-card-template]")
+
+
+
+
+    function fetchReq(event, searchBox) {
+        // console.log(searchBox.value)
+        const userInput = searchBox.value
+        document.getElementById('datalistOptions').innerHTML = ""
+        if (event.key === "Enter") {
+            fetch('https://api.magicthegathering.io/v1/cards?' + new URLSearchParams({
+                name: userInput,
+                // pageSize: 10
+            }))
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response is not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const currentOptions = document.getElementById('datalistOptions');
+                currentOptions.innerHTML = "";
+                data.cards.forEach(card => {
+
+                    const currentValue = (Array.from(currentOptions.childNodes)[0]) ? (Array.from(currentOptions.childNodes)[0].value) : "";
+                    console.log(card.name)
+                    // if (currentValue !== card.name) {
+                    //     const option = document.createElement('option');
+                    //     option.value = card.name;
+                    //     currentOptions.appendChild(option);
+                    //     console.log(option.value);
+                    // }
+                       if (currentValue.length !== card.name) {
+                            const option = document.createElement('option');
+                            option.value = card.name;
+                            currentOptions.appendChild(option);
+                            console.log(option.value)
+                       }
+                });
+
+
+
+                
+                
+                // const options = Array.from(document.getElementById("datalistOptions").children);
+                const options = document.getElementById('datalistOptions').children;
+                // console.log(typeof options);
+                // console.log(options);
+                // console.log(Object.values(options));
+                Object.values(options).forEach(value => {
+                    // console.log(value.value);
+                })
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+        }
+    }
+
+    // function fetchReq(event, searchBox) {
+    //     // console.log(searchBox.value)
+    //     const userInput = searchBox.value
+    //     document.getElementById('datalistOptions').innerHTML = ""
+    //     if (event.key === "Enter") {
+    //         fetch('https://api.magicthegathering.io/v1/cards?' + new URLSearchParams({
+    //             name: userInput,
+    //             pageSize: 10
+    //         }))
+    //         .then(response => {
+    //             if (!response.ok) {
+    //                 throw new Error('Network response is not ok');
+    //             }
+    //             return response.json();
+    //         })
+    //         .then(data => {
+    //             document.getElementById('datalistOptions').innerHTML = ""
+    //             data.cards.forEach(card => {
+    //                 const option = document.createElement('option')
+    //                 option.value = card.name
+    //                 document.getElementById('datalistOptions').appendChild(option)
+
+    //             });
+    //             // const options = Array.from(document.getElementById("datalistOptions").children);
+    //             const options = document.getElementById('datalistOptions').children
+    //             console.log(typeof options)
+    //             console.log(options)
+    //             console.log(Object.values(options))
+    //             Object.values(options).forEach(value => {
+    //                 console.log(value.value)
+    //                 console.log(document.getElementById('datalistOptions'))
+    //             })
+    //         })
+    //         .catch(error => {
+    //             console.error('There was a problem with the fetch operation:', error);
+    //         });
+    //     }
+    // }
